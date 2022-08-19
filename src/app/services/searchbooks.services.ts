80c225299c74b3eb94d6c3641dd2c12a6f027fa1
Pramod Naik <pramodnaik221@gmail.com>
2022-08-19 17:20:15 +0530

    Search in working

--- src/app/services/searchbooks.services.ts
+++ src/app/services/searchbooks.services.ts
@@ -14,14 +14,14 @@ export class SearchBooksService {
 
   //Get all books
   SearchBook(serachCriteria: ReaderSearchCriteria):Observable<ReaderSearchCriteria[]>{
+
     let queryParams = new HttpParams();
-    queryParams.append("bookTitle", serachCriteria.BookTitle);
-    // queryParams.append("category", serachCriteria.Category);
-    // queryParams.append("author", serachCriteria.Author);
-    // queryParams.append("publisher", serachCriteria.Publisher);
-    // queryParams.append("price", serachCriteria.Price);
-    console.log(serachCriteria.BookTitle);
-    console.log(queryParams);
-   return this.http.get<ReaderSearchCriteria[]>(this.baseUrl, {params : queryParams});
+      queryParams = queryParams.append("bookTitle",serachCriteria.BookTitle);
+      queryParams = queryParams.append("category",serachCriteria.Category);
+      queryParams = queryParams.append("author",serachCriteria.Author);
+      queryParams = queryParams.append("price",serachCriteria.Price);
+      queryParams = queryParams.append("publisher",serachCriteria.Publisher);
+
+      return this.http.get<ReaderSearchCriteria[]>(this.baseUrl,{params:queryParams});
   }
 }
